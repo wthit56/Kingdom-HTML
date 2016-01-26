b48cee01e58640e0ade12e53e98a800d9e71440e
Thomas Giles <wthit56@googlemail.com>
2016-01-26 20:27:51 +0000

    Game keys input handling
    - king moves, flips, idles, eats after 1 second

--- loop.js
+++ loop.js
@@ -35,19 +35,62 @@ var draw, update; (function() {
 		//*/
 	}
 
-	var uTime = 0;
+	var uTime = 0, uDelta = 0, ue;
+	var sTime = 0;
 	update = function(time) {
+		var _sim = sim[
+			state === STATES.game ? "game" :
+				"none"
+		];
 		switch (state) {
 			case STATES.game:
-				/*
-				var delta = time - uTime;
-				graphics.cobblestones.position.x += delta / (1000 / 10);
-				//*/
+				while (input.length > 0) {
+					ue = input.shift();
+					_sim(ue.time - sTime, ue.time);
+					sTime = ue.time;
+					if (ue.type.substr(0, 3) === "key") {
+						input.keys[input.keys.byCode[ue.keyCode]] = (ue.type === "keydown");
+					}
+				}
+				_sim(time - sTime, time);
+				sTime = time;
 				
 				graphics.king.animation.update(time);
 				break;
 		}
 		
 		uTime = time;
+	};
+	
+	input.keys = {
+		byCode: {
+			37: "left", 39: "right", 28: "up", 40: "down",
+			16: "shift", 17: "control"
+		}
+	};
+	
+	var sim = {
+		game: function(delta, time) {
+			window.time = time;
+			if (input.keys.left && !input.keys.right) {
+				graphics.king.position.x -= delta * graphics.king.speed;
+				graphics.king.flip = graphics.FLIP.x;
+				graphics.king.animation.startAnimation("walk", time);
+			}
+			else if (input.keys.right && !input.keys.left) {
+				graphics.king.position.x += delta * graphics.king.speed;
+				graphics.king.flip = graphics.FLIP.none;
+				graphics.king.animation.startAnimation("walk", time);
+			}
+			else if ((graphics.king.animation.current === graphics.king.animation.idle) && (time - graphics.king.animation.startTime > 1000)) {
+				console.log("eat");
+				graphics.king.animation.startAnimation("eat", time);
+			}
+			else if (graphics.king.animation.current !== graphics.king.animation.eat) {
+				graphics.king.animation.startAnimation("idle", time);
+				
+			}
+		},
+		none: function() { }
 	};
 })();
